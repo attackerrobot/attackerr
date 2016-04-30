55e41714196b8d69791b7ed498fc676cc98f8250
attackerrobot <amirattacker1382@gmail.com>
2016-04-30 23:29:53 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -1,5 +1,5 @@
 package.path = package.path .. ';.luarocks/share/lua/5.2/?.lua'
-  ..';.luarocks/share/lua/5.2/?/init.lua'
+  ..';.luarocfy
 package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
 
 require("./bot/utils")
